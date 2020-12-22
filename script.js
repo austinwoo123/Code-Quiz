f0ab7e3cbf8b3c4a12fb7194adc8b6d84bcaf90e
Austin W <austinwoo123@gmail.com>
2020-12-21 18:16:44 -0800

    finish some of the styling and begin javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+var questions = [{
+    prompt: "What is the most common natural object to be mistaken for a UFO?",
+    options: ["Airplane", "Bird", "Venus", "Moon"],
+    answer: "Venus"
+},
+{
+    prompt: "The average person does what thirteen times a day?",
+    options: ["Burps", "Sneeze", "Smile", "Laugh"],
+    answer:
+}
