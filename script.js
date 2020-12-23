5242f5dbaf4fcc1e8cb6da07c31ca187f281ecd1
Austin W <austinwoo123@gmail.com>
2020-12-22 16:45:20 -0800

    finish questions and add functions starting with quizStart

--- script.js
+++ script.js
@@ -5,20 +5,39 @@ var timer;
 
 var questions = [{
     prompt: "What is the most common natural object to be mistaken for a UFO?",
-    options: ["Airplane", "Bird", "Venus", "Moon"],
+    options: ["airplane", "bird", "venus", "moon"],
     answer: "Venus"
 },
 {
     prompt: "The average person does what thirteen times a day?",
-    options: ["Burps", "Sneeze", "Smile", "Laugh"],
+    options: ["burps", "sneeze", "smile", "laugh"],
     answer: "laugh"
 },
 {
     prompt: "Where was the fortune cookie invented?",
-    options: ["Hong Kong", "Seatle", "New York", "San Francisco"],
+    options: ["hong Kong", "seatle", "new york", "san francisco"],
     answer: "San Francisco",
 
 },
 {
-    prompt: ""
+    prompt: "In Minnesota it is illegal to tease what type of animal?",
+    options: ["squirrel", "rabbit", "skunk", "dogs"],
+    answer: "skunk"
+}
+]
+
+function quizStart() {
+    timeRemain = 100;
+    document.getElementById("timeRemain").innerHTML = timeRemain;
+    timer = setInterval(function () {
+        timeRemain--;
+        document.getElementById("timeRemain").innerHTML = timeRemain;
+
+        if (timeRemain <= 0) {
+            clearInterval(timer);
+            gameFinish();
+        }
+
+    }, 1000);
+    next()
 }
